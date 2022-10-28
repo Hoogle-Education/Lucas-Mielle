06ce50832c5f0fa203e8de15e35b49e7c792f883
hgrafa <hugorafell@gmail.com>
2022-10-28 14:38:31 -0300

    Adding sort project

---
+++ samples/sorting/sort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "selection_sort.h"
+#include "bubble_sort.h"
+
+int main() {
+
+  int tamanho = 5;
+  int vetor[] = {29,10,14,37,14};
+  int vetor2[] = {5, 4, 3, 2, 1};
+
+  // selection_sort(vetor, tamanho);
+  bubble_sort(vetor2, tamanho);
+
+}
